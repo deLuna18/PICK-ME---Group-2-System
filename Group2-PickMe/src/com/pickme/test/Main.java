89577851c85d5b1f10828de6d043102a07017a73
Alexa <113959920+deLuna18@users.noreply.github.com>
2023-12-17 22:31:52 +0800

    Delete Group2-PickMe/src/com/pickme/test/Main.java

--- Group2-PickMe/src/com/pickme/test/Main.java
+++
@@ -1,9 +0,0 @@
-package com.pickme.test;
-
-public class Main {
-    public static void main(String[] args) {
-        Gender gender = new Gender();
-
-        gender.getFrame().setVisible(true);
-    }
-}
