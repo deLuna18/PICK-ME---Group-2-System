856e62d1d064f43eb654d35d5abe778c7cbcfdcc
Alexa <113959920+deLuna18@users.noreply.github.com>
2023-12-17 06:31:08 -0800

    Add files via upload

---
+++ Group2-PickMe/src/com/pickme/test/Main.java
@@ -0,0 +1,9 @@
+package com.pickme.test;
+
+public class Main {
+    public static void main(String[] args) {
+        Gender gender = new Gender();
+
+        gender.getFrame().setVisible(true);
+    }
+}
